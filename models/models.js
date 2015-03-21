8bafe957b0bed27f6221ac23f47ba84d3c00e0de
Jim Ng <jam_ng2000@hotmail.com>
2015-03-22 03:49:57 +0800

    Add some functions to access the file system

--- models/models.js
+++ models/models.js
@@ -1,7 +1,10 @@
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 mongoose.connect('mongodb://localhost:27017/fyp');
 
-User = mongoose.model('uesrs', {name: String, pw: String});
+User = mongoose.model('users', {name: String, pw: String});
+FileSystem = mongoose.model('fs', {name: String, root: Schema.Types.Mixed});
+
 exports.createUser = function (name, pw, callback){
 	var user = new User({name: name, pw: pw});
 	user.save(function(err){
@@ -10,7 +13,46 @@ exports.createUser = function (name, pw, callback){
 };
 
 exports.findUser = function (name, callback){
-	User.find({}, function(err, user){
+	User.find({name: name}, function(err, user){
 		callback(err, user);
+	});
+};
+
+exports.deleteUser = function (name, callback){
+	User.remove({name: name}, function(err){
+		callback(err);
+	});
+};
+
+exports.init = function(name, callback){
+	var entry = new FileSystem({name: name, root: {type: "dir", children:[]}});
+	entry.save(function(err){
+		callback(err);
+	});
+}
+
+exports.createFolder = function (name, path, callback){
+	FileSystem.find({name: name}, function(err, fs){
+		var cur = fs[0].root;
+		var stack = [cur];
+		for (var i = 0; i < path.length; i++){
+			if (cur.children[path[i]] === undefined){ // if there is no directory, create one
+				cur.children[path[i]] = {type: "dir", children: []};
+			}
+			cur = cur.children[path[i]];
+			stack.push(cur);
+		}
+		for (var i = path.length - 1; i >= 0; i--){
+			stack[i].children[path[i]] = stack[i + 1];
+			console.log(stack[i]);
+		}
+		var entry = new FileSystem({name: name, root: stack[0]});
+		console.log(entry);
+	});
+};
+
+exports.dumpStructure = function (name, callback){
+	FileSystem.find({name: name}, function(err, fs){
+		callback(err, fs);
 	});
 }
