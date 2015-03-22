ed6e2af0a29a0e8002f0d12fec70d05695d77545
wing <sktgintokii@gmail.com>
2015-03-22 19:06:27 +0800

    init

--- public/lib/main.js
+++ public/lib/main.js
@@ -1,3 +1,47 @@
+function getQueryString() {
+  	var result = {}, queryString = location.search.slice(1),
+      re = /([^&=]+)=([^&]*)/g, m;
+
+  	while (m = re.exec(queryString)) {
+    	result[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
+  	}
+
+  	return result;
+}
+
+
+function init(){
+	var dir = getQueryString()['dir'];
+
+	if (dir === 'undefined'){
+		var uid = document.querySelector('#user-id');
+		superagent
+			.get('/fs/getRoot')
+			.query({uid: uid})
+			.end(function (err, res){
+				if (err){
+					console.log(err);
+				} else {
+
+				}
+
+			});
+	} else {
+		superagent
+			.get('/fs/lsDir')
+			.query({fileid: dir})
+			.end(function (err, res){
+				if (err){
+					console.log(err);
+				} else {
+
+				}
+			})
+	}
+
+	
+};
+
 function logoutHandler(e){
 	e.preventDefault();
 
@@ -9,4 +53,5 @@ function logoutHandler(e){
 	form.submit();
 }
 
+init();
 document.querySelector('#logout-opt').addEventListener("click", logoutHandler);
--- routes/frontend.js
+++ routes/frontend.js
@@ -1,12 +1,22 @@
-var express = require('express');
+var express = require('express'),
+	bodyParser = require('body-parser'),
+	session = require('express-session'),
+	cookieParser = require('cookie-parser'),
+	expressValidator = require('express-validator');
 
 module.exports = function (){
+	var path = '/';
 	var app = express.Router();
 
+	app.use(bodyParser.urlencoded({extended:true}));
+	app.use(cookieParser());
+
 	app.get('/', function (req, res){
+		console.log(req.session);
 		res.render('main-panel', {
 			title: 'main',
-			layout: 'main'
+			layout: 'main',
+			uid: req.session.username
 		});
 	});
 
