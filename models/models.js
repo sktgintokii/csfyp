c527d46c40894dbe06fd0ff1bd26792379669967
Jim Ng <jam_ng2000@hotmail.com>
2015-04-01 18:55:30 +0800

    Fix bugs

--- models/models.js
+++ models/models.js
@@ -37,6 +37,27 @@ exports.deleteUser = function (name, callback){
 	});
 };
 
+exports.addGoogleDrive = function (uid, code, callback){
+	googleapis.getAccessToken(code, function(err, token){
+		if (err) callback(err);
+		else if (token.refresh_token == undefined) callback("Refresh token not found");
+		else{
+			googleapis.getAccessToken(code, function(tokenInfo){
+				Token.findOne({owner: tokenInfo.issued_to}, function(err, entry){
+					if (err) callback(err);
+					else if (entry != null) callback("Duplicate Google Account");
+					else{
+						var accessToken = new Token({uid: uid, platform: "Google", owner: tokenInfo.issued_to, Token: token});
+						accessToken.save(function(err){
+							callback(err);
+						})
+					}
+				})
+			});
+		}
+	});
+};
+
 exports.initDir = function(uid, callback){
 	var root = new File({uid: "root", type: "dir", children: [], parent: null, owner: uid});
 	var entry = new FileSystem({uid: uid, root: root._id});
--- routes/addDrive.js
+++ routes/addDrive.js
@@ -1,13 +1,23 @@
-var driveManager = require('../models/driveManager.js');
+var models = require('../models/models.js');
 var express = require('express');
 var app = express.Router();
 
 // IN: code
 // OUT: err
+/*
 app.get('/google', function (req, res){
 	driveManager.addGoogleDrive(req.session.uid, req.query.code, function(err){
 		res.send({err: err});
 	}
+});
+*/
+
+// IN: code
+// OUT: err
+app.get('/google', function (req, res){
+	models.addGoogleDrive(req.query.uid, req.query.code, function(err){
+		res.send({err: err});
+	});
 });
 
 module.exports = app;
