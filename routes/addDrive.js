c2a35b1bf61253d6b553c238735111337abbc387
wing <sktgintokii@gmail.com>
2015-04-07 03:21:20 +0800

    addDrive redirect

--- routes/addDrive.js
+++ routes/addDrive.js
@@ -21,24 +21,41 @@ app.get('/google', function (req, res){
 	});
 });
 */
-app.use('/addDrive/google', function (req, res){
+
+// intermediate layer for getting session
+app.use('/addDrive/getToken', function (req, res){
 	models.addGoogleDrive(req.session.uid, req.query.code, function(err){
 		if (err){
-			res.render('drive-add-err', {
-				title: 'error',
-				layout: false,
-				err: err
-			});
+			res.redirect('/addDrive/error?err=' + JSON.stringify(err));
 		} else {
-			res.render('drive-add-success', {
-				title: 'success',
-				layout: false
-			});
+			res.redirect('/addDrive/success');
 		}
 	});
+})
 
-
+app.get('/addDrive/google', function (req, res){
+	var code = req.query.code;
+	if (code){
+		res.redirect('/addDrive/getToken?code=' + code);
+	} else {
+		res.redirect('/addDrive/error?err=' + JSON.stringify(err));
+	}
 });
+
+app.get('/addDrive/success', function (req, res){
+	res.render('drive-add-success', {
+		title: 'success',
+		layout: false
+	});
+})
+
+app.get('/addDrive/error', function (req, res){
+	res.render('drive-add-err', {
+		title: 'error',
+		layout: false,
+		err: err
+	});
+})
 
 
 module.exports = app;
