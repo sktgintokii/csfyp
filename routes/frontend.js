1a9146505280b79677a270b48951b2e4ad4651bf
Jim Ng <jam_ng2000@hotmail.com>
2015-04-09 17:44:30 +0800

    updated add google drive UI

--- routes/frontend.js
+++ routes/frontend.js
@@ -7,6 +7,7 @@ var express = require('express'),
 module.exports = function (){
 	var path = '/';
 	var app = express.Router();
+	var googleLogoutLink = "https://www.google.com/accounts/Logout?continue=";
 	var googleAuthUrl = "https://accounts.google.com/o/oauth2/auth?access_type=offline&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdrive&response_type=code&client_id=280286530527-lh0iqa2kh1r9si7v7v84ldn181n4caca.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2FaddDrive%2Fgoogle"
 
 	app.use(bodyParser.urlencoded({extended:true}));
@@ -18,7 +19,7 @@ module.exports = function (){
 		res.render('main-panel', {
 			title: 'main',
 			layout: 'main',
-			googleAuthUrl: googleAuthUrl,
+			googleAuthUrl: googleLogoutLink + encodeURIComponent(googleAuthUrl),
 			uid: req.session.username
 		});
 	});
