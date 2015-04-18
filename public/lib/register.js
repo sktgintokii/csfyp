a8f0d85b9bb7828f3d0c9fda3c8a255cce6a274f
wing <sktgintokii@gmail.com>
2015-04-18 14:31:04 +0800

    front-end register

--- public/lib/register.js
+++ public/lib/register.js
@@ -1,3 +1,47 @@
+function serializeFormData(form) {
+	return [].map.call(form.elements, function(el) {
+		if (el.name && !el.disabled 
+				&& (!el.type || el.type.toLowerCase() !== 'checkbox' || el.checked)) {
+			if (el.tagName === 'SELECT' && el.hasAttribute('multiple'))
+				return [].map.call(el.selectedOptions, function(o) {
+					return [el.name, o.value].map(encodeURIComponent).join('=');
+				}).join('&');
+			return [el.name, el.value].map(encodeURIComponent).join('=');
+		}
+	}).join('&');
+};
+
+function onSubmitHandler(e) {
+	// Disable default form submission to prevent page load
+	e.preventDefault();
+
+	var valid = validate();
+	if (!valid) {
+		alert('Invalid Input');
+		return false;
+	} 
+
+	// Reference: http://visionmedia.github.io/superagent/#post-/%20put%20requests
+	superagent
+		.post(this.getAttribute('action'))
+		.send(serializeFormData(this))
+		.end(function (res) {
+			if (res.error) {
+				if (res.body.err) {
+					res.body.err.forEach(function(input){
+						alert(input.msg);
+					});
+				}
+				alert(res.body.err || res.error);
+				return console.error(res.body.err || res.error);
+			}
+
+			alert('Account is created Successfully');
+			// refresh the page with latest results
+			location.reload();
+		});
+}
+
 function validate(e){
 	var valid;
 
@@ -28,16 +72,13 @@ function validate(e){
 	return valid;
 }
 
-function submitFormHandler(e){
-	var valid = validate();
-	if (!valid){
-		alert('Error: Passwords mismatched!');
-		e.preventDefault();
-		return false;
-	}
-	
-}
 
 document.querySelector('#password-input').addEventListener('change', validate);
 document.querySelector('#confirm-password-input').addEventListener('change', validate);
-document.querySelector('form').addEventListener('submit', submitFormHandler);
+document.querySelector('form').addEventListener('submit', onSubmitHandler);
+
+
+
+
+
+
--- routes/accountRouter.js
+++ routes/accountRouter.js
@@ -1,7 +1,30 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
+
+var inputPattern = {
+	userid: /\w+/
+};
 
 module.exports = function (){
 	var app = express.Router();
+	app.use(bodyParser.urlencoded({extended:true}));
+	app.use(expressValidator());
+
+
+	app.post('/api/register', function (req, res){
+		req.checkBody('userid', 'Invalid user Id')
+			.matches(inputPattern.userid);
+
+
+		// quit processing if encountered an input validation error
+		var errors = req.validationErrors();
+		if (errors) {
+			return res.status(400).json({'inputError': errors}).end();
+		}
+
+		
+	});
 
 	// Expected route: /account/register
 	app.use('/register', function (req, res){
@@ -10,6 +33,7 @@ module.exports = function (){
 			layout: 'register',
 		});
 	});
+
 
 	return app;
 }
